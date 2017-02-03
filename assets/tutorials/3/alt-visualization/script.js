a0eda20e6ca3a80f8315e056d8ae46b653ebb846
RainingFlamebows <sshih16@berkeley.edu>
2017-02-02 22:33:27 -0800

    changed visualization/interactive gif into a slideshow so users can view at their own pace.

---
+++ assets/tutorials/3/alt-visualization/script.js
@@ -0,0 +1,23 @@
+var prefix = "/blog/assets/tutorials/3/alt-visualization/slides/slide-";
+var numslides = 10;
+var suffix = ".png";
+  
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var img = document.getElementById("alt-visual");
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > numslides) {slideIndex = 0}    
+  if (n < 0) {slideIndex = slideIndex}
+  img.src = prefix + slideIndex.toString() + suffix;
+}
