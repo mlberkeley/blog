4159a6eaf74e29dda83f479ad24036dde98850b1
RainingFlamebows <sshih16@berkeley.edu>
2017-06-19 20:30:15 -0700

    Got visual done. Added images and captions.

--- assets/tutorials/4/visual.js
+++ assets/tutorials/4/visual.js
@@ -1,5 +1,12 @@
+// dimensions of each frame
 var imgWidth = 600;
-var imgHeight = 400;
+var imgHeight = 450;
+
+// dimensions of image in frames
+var rows = 24;
+var cols = 5;
+
+var topOffset = -30;
 
 var startVal = +document.getElementById("image_id").getAttribute("start");
 showVal(startVal);
@@ -18,10 +25,10 @@ function showImage(newVal) {
   div.style.height = imgHeight.toString() + "px";
   
   var img = document.getElementById("predPic");
-  img.style.top = "-30px";
-  img.style.left = (-1 * (+newVal) * imgWidth).toString() + "px";
+  img.style.top = ((-1 * (+newVal) * imgHeight) % (rows * imgHeight)).toString() + "px";
+  img.style.left = ((-1 * parseInt(+newVal / rows) * imgWidth) ).toString() + "px";
   
   var img = document.getElementById("errPic");
-  img.style.top = "-30px";
-  img.style.left = (-1 * (+newVal) * imgWidth).toString() + "px";
+  img.style.top = ((-1 * (+newVal) * imgHeight) % (rows * imgHeight)).toString() + "px";
+  img.style.left = ((-1 * parseInt(+newVal / rows) * imgWidth) ).toString() + "px";
 }
